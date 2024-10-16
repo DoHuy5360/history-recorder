c244a79331fb33add46df4246559ea04bcd9e580
Do Huy <huy.d@vlu.edu.vn>
2024-10-16 17:01:43 +0700

    update: Matching data to real time and display

--- src/ui/calendar/View.tsx
+++ src/ui/calendar/View.tsx
@@ -8,9 +8,22 @@ const currentDay = moment().date();
 const daysInMonth = moment().daysInMonth();
 const currentYear = moment().year();
 const arrayOfDays = Array.from({ length: daysInMonth }, (v, k) => ++k);
-
-const histories = [
-	{
+type History = {
+	[Key: number]: Time;
+};
+type Task = {
+	id: string;
+	name: string;
+	status: string;
+};
+type Time = {
+	day: number;
+	month: number;
+	year: number;
+	tasks: Task[];
+};
+const histories: History = {
+	1: {
 		day: 1,
 		month: 9,
 		year: 2024,
@@ -21,13 +34,13 @@ const histories = [
 			{ id: "t2", name: "Task 2", status: "Done" },
 		],
 	},
-	{
+	2: {
 		day: 2,
 		month: 9,
 		year: 2024,
 		tasks: [{ id: "t3", name: "Task 3", status: "Done" }],
 	},
-	{
+	3: {
 		day: 3,
 		month: 9,
 		year: 2024,
@@ -36,7 +49,7 @@ const histories = [
 			{ id: "t6", name: "Task 6", status: "Done" },
 		],
 	},
-];
+};
 
 function View() {
 	const [startDate, setStartDate] = useState<number | null>(null);
@@ -77,19 +90,20 @@ function View() {
 	return (
 		<div>
 			<div className="table_border_style grid grid-cols-7 border-[1px] m-4">
-				{histories.map((time, index) => {
+				{arrayOfDays.map((day: number, index) => {
+					const time: Time = histories[day as keyof History];
 					return (
 						<div key={index}>
 							<div
 								onClick={() => {
-									handleSelectDays(time.day);
+									handleSelectDays(day);
 								}}
-								className={`${time.day === currentDay && "border-red-400"}
-								${startDate && startDate === time.day && "bg-green-200"}
-								${endDate && endDate === time.day && "bg-blue-200"}
-								${startDate && endDate && time.day > startDate && time.day < endDate && "bg-yellow-200"}
-										cell_border_style border-[1px] p-4 select-none`}>
-								{time.day}
+								className={`${day === currentDay && "border-red-400"}
+								${startDate && startDate === day && "bg-green-200"}
+								${endDate && endDate === day && "bg-blue-200"}
+								${startDate && endDate && day > startDate && day < endDate && "bg-yellow-200"} h-full cell_border_style border-[1px] p-2 select-none`}>
+								<div>{day}</div>
+								{time && <div className="bg-red-500 w-fit px-2 py-1 rounded-full grid items-center text-white text-xs font-bold ml-auto">{time.tasks.length}</div>}
 							</div>
 						</div>
 					);
@@ -118,25 +132,28 @@ function View() {
 					</tr>
 				</thead>
 				<tbody>
-					{histories.slice(startDate ? startDate - 1 : endDate ? endDate - 1 : 0, endDate ? endDate : startDate ? startDate : 0).map((time, index) => {
-						amountOfTaskSelected += time.tasks.length;
-						return (
-							<Fragment key={"fragment-" + index}>
-								<tr>
-									<td rowSpan={time.tasks.length}>{`${moment(`${time.year}-${time.month}-${time.day}`, "YYYY-M-DD").format("DD/MM/YYYY")}`}</td>
-									<td>{time.tasks[0].name}</td>
-									{isShowStatusColumn && <td>{time.tasks[0].status}</td>}
-								</tr>
-								{time.tasks.slice(1, time.tasks.length).map((task, index) => {
-									return (
-										<tr key={"r" + index}>
-											<td>{task.name}</td>
-											{isShowStatusColumn && <td>{task.status}</td>}
-										</tr>
-									);
-								})}
-							</Fragment>
-						);
+					{arrayOfDays.slice(startDate ? startDate - 1 : endDate ? endDate - 1 : 0, endDate ? endDate : startDate ? startDate : 0).map((day, index) => {
+						const time = histories[day as keyof History];
+						if (time) {
+							amountOfTaskSelected += time.tasks.length;
+							return (
+								<Fragment key={"fragment-" + index}>
+									<tr>
+										<td rowSpan={time.tasks.length}>{`${moment(`${time.year}-${time.month}-${time.day}`, "YYYY-M-DD").format("DD/MM/YYYY")}`}</td>
+										<td>{time.tasks[0].name}</td>
+										{isShowStatusColumn && <td>{time.tasks[0].status}</td>}
+									</tr>
+									{time.tasks.slice(1, time.tasks.length).map((task, index) => {
+										return (
+											<tr key={"r" + index}>
+												<td>{task.name}</td>
+												{isShowStatusColumn && <td>{task.status}</td>}
+											</tr>
+										);
+									})}
+								</Fragment>
+							);
+						}
 					})}
 				</tbody>
 				<tfoot>
